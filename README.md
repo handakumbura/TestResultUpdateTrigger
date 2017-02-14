16a936a5b63788d82db8eecd8df2f1da405c6cc0
handakumbura <dumiduh@gmail.com>
2017-02-14 15:36:41 +0530

    adds invocation logic.

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # TestResultUpdateTrigger
-This bash script is writ to trigger an internal build test case update mechanism as a Jenkins post step. Internal script variables,
+This bash script was writ to trigger an internal build test case update mechanism as a Jenkins post job step. Internal script variables,
 
 Variable | Purpose
 --- | ---
 LOG_FILE_LOCATION | captures the fully qualified path/name of the file that will be used to log script output.
-HTTP_TIMEOUT | timeout value used for HTTP requests, set in seconds.
-REMOTE_SERVER_IP | ip address or the hostname of the remote server.
-MSG | a variable used to pass messages to the logging function. 
-ENDPOINT | URL of the test result update servlet.
+HTTP_TIMEOUT | captures the timeout value used for HTTP requests, set in seconds.
+REMOTE_SERVER_IP | captures the ip address or the hostname of the remote server.
+REQUEST_PAYLOAD | a variable used to hold the generated request payload. 
+RESPONSE | captures the name of a temporary file that gets created to assert the service invocation response.
+ASSERTION_VALUE | captures a string that is looked for in the service response to validate successful service invocation.
+MSG | a variable used to pass messages to logging function. 
+ENDPOINT | captures the URL of the test result update service.
--- TestResultsUpdateTrigger.sh
+++ TestResultsUpdateTrigger.sh
@@ -3,11 +3,14 @@
 ##################
 # Global Variables
 ##################
-LOG_FILE_LOCATION="/home/$USER/testresultstrigger.log" #log file location
+LOG_FILE_LOCATION="/home/$USER/testresultstrigger.log" #log file location.
 HTTP_TIMEOUT=120 #time out value used for HTTP requests, set in seconds.
 REMOTE_SERVER_IP=localhost
+REQUEST_PAYLOAD="Oops!"
+RESPONSE="$(pwd)/out"
+ASSERTION_VALUE="HTTP/1.1 200 OK"
 MSG="Oops!"
-ENDPOINT=""
+ENDPOINT="https://10.100.5.74:8243/testresultupdater"
 
 ##################
 # Functions
@@ -23,7 +26,6 @@ failover()
 {
  return
 }
-
 
 ##################
 # Checks
@@ -48,7 +50,7 @@ if [ -z $BUILD_NUMBER ]; then
  exit 1;
 fi
 
-#check availability of cURL HTTP client
+#check availability of cURL HTTP client.
 dpkg -s curl &> /dev/null
 if [ $? -gt 0 ]; then
  MSG="cURL HTTP client was not found. Please install it on the system."
@@ -56,7 +58,7 @@ if [ $? -gt 0 ]; then
  exit 1;
 fi
 
-#check remote server availability
+#check remote server availability.
 ping -q -c 2 -w $HTTP_TIMEOUT $REMOTE_SERVER_IP &> /dev/null
 if [ $? -gt 0 ]; then
  MSG="Remote server is inaccessible. Check server accessibility and remote server values set in script."
@@ -68,6 +70,27 @@ fi
 MSG="Preliminary checks passed. Continuing with the script run."
 echo "$MSG"
 logmessage $MSG
+MSG="Parameters for the script run: timeout- $HTTP_TIMEOUT, remote ip- $REMOTE_SERVER_IP, EPR - $ENDPOINT, response file- $RESPONSE, assertion- $ASSERTION_VALUE"
+logmessage $MSG
 
+###################
+# Invocation Logic
+###################
 
-#------------------
+REQUEST_PAYLOAD="$BUILD_NUMBER/$POM_ARTIFACTID/$POM_VERSION"
+#Making HTTP request to the result update servlet.
+curl -X GET -k -i -f -m $HTTP_TIMEOUT -H "Accept: application/json" "$ENDPOINT/$REQUEST_PAYLOAD" > out
+cat "$RESPONSE" | grep "$ASSERTION_VALUE"
+
+#Asserting HTTP response.
+if [ $? -gt 0 ]; then
+ MSG="Response assertion failed. The response is availble at $RESPONSE"
+ logmessage $MSG
+ exit 1
+fi
+rm -r "$RESPONSE"
+
+#Exiting script.
+MSG="Test Result Update service was invoked with the project values. Script run successful."
+logmessage $MSG
+exit 0
