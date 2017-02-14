3a0dea6eb43c4548da7af02b45ee44bd03ad34e2
handakumbura <dumiduh@gmail.com>
2017-02-14 11:54:40 +0530

    adds a readme file and a few more checks to the script.

--- TestResultsUpdateTrigger.sh
+++ TestResultsUpdateTrigger.sh
@@ -3,48 +3,71 @@
 ##################
 # Global Variables
 ##################
-#LOG_FILE_LOCATION=`/home/$USER/resultsupdatetrigger.log` #log file location
+LOG_FILE_LOCATION="/home/$USER/testresultstrigger.log" #log file location
 HTTP_TIMEOUT=120 #time out value used for HTTP requests, set in seconds.
 REMOTE_SERVER_IP=localhost
-MSG="unset"
-
+MSG="Oops!"
+ENDPOINT=""
 
 ##################
-#functions
+# Functions
 ##################
 logmessage()
 {
  DATELOCAL=`date`
- echo "$DATELOCAL : $MSG" >> ../testout
+ echo "$DATELOCAL : $MSG" >> "$LOG_FILE_LOCATION"
+ return
+}
+
+failover()
+{
  return
 }
 
 
-
-
 ##################
-#checks
+# Checks
 ##################
 
 #checks for the needed env variables.
 if [ -z $POM_ARTIFACTID ]; then
- MSG="artifactId was not set as an environmental variable." 
+ MSG="ArtifactId was not set as an environmental variable." 
  logmessage $MSG
  exit 1;  
 fi
 
 if [ -z $POM_VERSION ]; then
- MSG="pom version was not set as an environmental variable."
+ MSG="POM version was not set as an environmental variable."
  logmessage $MSG
  exit 1;
 fi
 
-#check for remote server availability
-ping -q -c 2 -w $HTTP_TIMEOUT $REMOTE_SERVER_IP
+if [ -z $BUILD_NUMBER ]; then
+ MSG="Build number was not set as an environmental variable."
+ logmessage $MSG
+ exit 1;
+fi
+
+#check availability of cURL HTTP client
+dpkg -s curl &> /dev/null
 if [ $? -gt 0 ]; then
- MSG="remote server is inaccessible. Check server accessibility and remote server values set in script."
+ MSG="cURL HTTP client was not found. Please install it on the system."
  logmessage $MSG
  exit 1;
 fi
 
-echo "Preliminary checks passed. Continuing with the script run."
+#check remote server availability
+ping -q -c 2 -w $HTTP_TIMEOUT $REMOTE_SERVER_IP &> /dev/null
+if [ $? -gt 0 ]; then
+ MSG="Remote server is inaccessible. Check server accessibility and remote server values set in script."
+ logmessage $MSG
+ exit 1;
+fi
+
+
+MSG="Preliminary checks passed. Continuing with the script run."
+echo "$MSG"
+logmessage $MSG
+
+
+#------------------
